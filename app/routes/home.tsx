f18f353b8810bdc4d1d726f9bdb7777b8f3e0135
akshay <baviskarakshay0702@gmail.com>
2026-03-23 19:25:39 +0530

    implement drag and drop uploader

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import Upload from "../../components/Upload";
 import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
 import Button from "../../components/ui/Button";
+import {useNavigate} from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +13,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const navigate = useNavigate();
+
+    const handleOnComplete = async (base64Image: string) => {
+        const newId = Date.now().toString();
+        navigate(`/visualizer/${newId}`);
+        return true;
+    }
+
   return (
       <div className="home">
         <Navbar />
@@ -49,7 +59,7 @@ export default function Home() {
                           <p>Supports .jpg, .png, formats upto 10MB</p>
                       </div>
 
-                      <p>Upload Images</p>
+                      <Upload onComplete={handleOnComplete}/>
                   </div>
               </div>
           </section>
